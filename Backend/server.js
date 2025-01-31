80a0fa6bd7c0cf8bc5c9e3ea0f517ecbfc35deac
Arjunjai10 <arjunjaishanmugam.pvn2023Icse@sece.ac.in>
2025-01-31 12:29:48 +0530

    6

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes'); 
 const app = express();
-
+const User=require('./models/User');
 
 app.use(express.json()); 
 app.use(cors()); 
